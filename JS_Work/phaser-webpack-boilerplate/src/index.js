a48dd927f0ceb7793eb0f2fa4867eff076627ced
AugustoCogollo <93163359+AugustoCogollo@users.noreply.github.com>
2023-03-29 16:21:35 -0600

    Floppy bird prototype
    - Collisions
    - Random assignment of y position to both upper and lower pipes

--- JS_Work/phaser-webpack-boilerplate/src/index.js
+++ JS_Work/phaser-webpack-boilerplate/src/index.js
@@ -1,28 +1,29 @@
-import Phaser, { Scene } from "phaser";
+import Phaser, { Game, Scene } from "phaser";
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-      default: "arcade",
-      arcade:{
-        gravity: {y: 350},
-        debug: true
-      }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade:{
+      gravity: {y: 350},
+      debug: true
+    }
     },
     scene: {
       preload,
       create,
       update
     }
-}
+  }
 
 const FlapVelocity = 250;
-const PipeSpawnTime = 5000;
+const PipeSpawnTime = 3000;
 
 let bird = null;
-let pipe = null;
+let upperPipe = null;
+let lowerPipe = null;
 let pipeTimeCount = 0;
 
 //Awake
@@ -35,6 +36,7 @@ function preload(){
 //Start
 function create(){
   this.add.image(0, 0, "sky").setOrigin(0);
+
   bird = this.add.sprite(100, config.height /2, "bird");
   this.physics.add.existing(bird);
   
@@ -45,22 +47,42 @@ function create(){
 function update(time, delta){
   pipeTimeCount += delta;
   if(pipeTimeCount >= PipeSpawnTime){
-    spawnPipes()
+    spawnPipes(this, bird);
+    pipeTimeCount = 0;
   }
+
+  this.physics.add.collider(bird, lowerPipe, function(){
+    alert("You lose");
+  });
+  this.physics.add.collider(bird, upperPipe, function(){
+    alert("You lose");
+  });
+  
 }
 
 function flap(){
   bird.body.velocity.y = -FlapVelocity;
 }
 
-function spawnPipes(){
-  let yPos = (Math.random() * 100) + 300;
-  pipe = this.add.sprite(config.width + 50, yPos);
-  this.physics.add.existing(pipe);
-  pipe.body.allowGravity = false;
-  pipe.body.immovable = true;
-  pipe.body.velocity.x = -100;
-  
+
+function spawnPipes(game, player){  
+
+  let spaceLeft = Phaser.Math.Between(player.height, player.height * 2);
+
+  let randomDistance = Phaser.Math.Between(0, 50);
+  lowerPipe = game.add.sprite(config.width + 50, randomDistance - spaceLeft, "pipe");
+  game.physics.add.existing(lowerPipe);
+  lowerPipe.body.allowGravity = false;
+  lowerPipe.body.immovable = true;
+  lowerPipe.body.velocity.x = -100;
+
+  randomDistance = Phaser.Math.Between(550, 600);
+  upperPipe = game.add.sprite(config.width + 50, randomDistance + spaceLeft, "pipe");
+  game.physics.add.existing(upperPipe);
+  upperPipe.body.allowGravity = false;
+  upperPipe.body.immovable = true;
+  upperPipe.body.velocity.x = -100;
 }
 
-new Phaser.Game(config);
+var game = new Phaser.Game(config);
+
